a9922d5c5d80707c3a4483143f228f8e83e06718
dhimantford <dshukla3@ford.com>
2023-06-05 11:31:06 +0530

    Changes -2

---
+++ conversationFlow.py
@@ -0,0 +1,25 @@
+def conversation(classification):
+    if classification["class"] == "greeting":
+        return greeting(classification["class"])
+    elif classification["class"] == "notCharging":
+        return vehicleChargingStatus(classification["class"])
+    elif classification["class"] == "chargeCompletionTime":
+        return vehicleChargeCompletionTime(classification["class"])
+    else:
+        return newQuery(classification["class"])
+
+
+def greeting(input):
+    return input
+
+
+def vehicleChargingStatus(input):
+    return input
+
+
+def vehicleChargeCompletionTime(input):
+    return input
+
+
+def newQuery(input):
+    return input
